b2cd12d43236eaaa8c531eba6d3659bd0e7c45df
UthfolGhosh <uthfolghosh9038@gmail.com>
2025-08-24 00:21:23 +0600

    connect database  into backend server with go and fetching data

---
+++ backend_mysql/go.mod
@@ -0,0 +1,10 @@
+module backend_mysql
+
+go 1.24.5
+
+require (
+	github.com/go-sql-driver/mysql v1.9.3
+	github.com/gorilla/mux v1.8.1
+)
+
+require filippo.io/edwards25519 v1.1.0 // indirect
---
+++ backend_mysql/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+var db *sql.DB
+
+type Album struct {
+	ID     int64   `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float32 `json:"price"`
+}
+
+func main() {
+	// Database config
+	cfg := mysql.NewConfig()
+	cfg.User = "root"
+	cfg.Passwd = "324648"
+	cfg.Net = "tcp"
+	cfg.Addr = "127.0.0.1:3306"
+	cfg.DBName = "records"
+
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to database!")
+
+	// Setup router
+	r := mux.NewRouter()
+	r.HandleFunc("/albums", getAlbums).Methods("GET")
+
+	log.Println("Server running at http://localhost:8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
+
+func getAlbums(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+
+	//get data from mysql server
+	rows, err := db.Query("SELECT  title FROM album")
+	if err != nil {
+		http.Error(w, "Failed to fetch data", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var albums []Album
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan( &alb.Title); err != nil {
+			http.Error(w, "Error scanning data", http.StatusInternalServerError)
+			return
+		}
+		albums = append(albums, alb)
+	}
+
+	if err := rows.Err(); err != nil {
+		http.Error(w, "Row iteration error", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(albums)
+}
